5496fa3c3caa9ef09cd79721eaa0132a43ea5802
Eric Acquah <ericacquah1720@gmail.com>
2024-02-10 22:59:28 +0000

    implemented a create order feature

--- models/clients.py
+++ models/clients.py
@@ -5,6 +5,7 @@ cooks on the platform
 """
 
 from models.users import UserBase
+from importlib import import_module
 
 
 class ClientModel(UserBase):
@@ -13,4 +14,26 @@ class ClientModel(UserBase):
 
     """
 
-    pass
+
+    def makeOrder(self, cookid, head, desc):
+        """
+        Creates a new order for the client
+
+        Args:
+            cookid (str): the cook to whom the order goes to
+            head (str): short description of the order
+            desc (str): detailed description of the order
+
+        Return:
+            True if successful else return False
+        """
+
+        order = import_module("models.orders", package="Cook4Me")
+        order = order.OrderModel()
+
+        clientId = self.getUser()
+        order.client_id = clientId['UserId']
+
+        stats = order.createOrder(cookid, head, desc)
+
+        return stats
